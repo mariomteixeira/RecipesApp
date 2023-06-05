d11f9e408fbf8d0d84a8358baeefc945d5bc5d73
Victor Schmoegel <victorschmoegel@gmail.com>
2023-06-05 19:35:51 -0300

    group-16-req-11

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,33 +1,70 @@
+import { useContext } from 'react';
+import { RecipesContext } from '../context/RecipesContext';
+
 function SearchBar() {
+  const { setSearchType, setSearchString, executeSearch } = useContext(RecipesContext);
+
+  const handleRadioChange = ({ target }) => {
+    setSearchType(target.value);
+  };
+
+  const handleSearchChange = ({ target }) => {
+    setSearchString(target.value);
+  };
+
+  const handleSearchClick = (event) => {
+    event.preventDefault();
+    executeSearch();
+  };
+
   return (
     <>
-      <button
+      <input
         className="search-bar"
         data-testid="ingredient-search-radio"
         type="radio"
+        value="ingredient"
+        onChange={ handleRadioChange }
       >
         Ingrediente
-      </button>
-      <button
+      </input>
+      <label htmlFor="ingredient-search-radio">Ingredient</label>
+
+      <input
         className="search-bar"
         data-testid="name-search-radio"
         type="radio"
+        value="name"
+        onChange={ handleRadioChange }
       >
         Name
-      </button>
-      <button
+      </input>
+      <label htmlFor="name-search-radio">Name</label>
+
+      <input
         className="search-bar"
         data-testid="first-letter-search-radio"
         type="radio"
+        value="first-letter"
+        onChange={ handleRadioChange }
       >
         First Letter
-      </button>
-      <button
+      </input>
+      <label htmlFor="first-letter-search-radio">First Letter</label>
+
+      <input
+        type="text"
+        onChange={ handleSearchChange }
+        placeholder="Buscar Receita"
+      />
+
+      <input
         className="search-bar"
         data-testid="exec-search-btn"
+        onClick={ handleSearchClick }
       >
         Executar
-      </button>
+      </input>
     </>
   );
 }
--- src/context/RecipesProvider.jsx
+++ src/context/RecipesProvider.jsx
@@ -1,16 +1,53 @@
 import PropTypes from 'prop-types';
-import React, { useState, useMemo } from 'react';
+import React, { useState, useMemo, useCallback } from 'react';
 import RecipesContext from './RecipesContext';
 
 export default function RecipesProvider({ children }) {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
+  const [searchType, setSearchType] = useState('');
+  const [searchString, setSearchString] = useState('');
+  const [searchResults, setSearchResults] = useState([]);
+
+  const executeSearch = useCallback(async () => {
+    if (searchString.length === 0) return;
+
+    let url = '';
+    if (searchType === 'ingredient') {
+      url = `https://www.themealdb.com/api/json/v1/1/filter.php?i=${searchString}`;
+    } else if (searchType === 'name') {
+      url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchString}`;
+    } else if (searchType === 'first-letter') {
+      if (searchString.length !== 1) {
+        global.alert('Your search must have only 1 (one) character');
+        return;
+      }
+      url = `https://www.themealdb.com/api/json/v1/1/search.php?f=${searchString}`;
+    }
+
+    try {
+      const response = await fetch(url);
+      if (!response.ok) throw new Error(`HTTP error ${response.status}`);
+      const data = await response.json();
+      setSearchResults(data.meals);
+    } catch (error) {
+      console.error('Failed to fetch:', error);
+    }
+  }, [searchType, searchString]);
+
   const store = useMemo(() => ({
     username,
     setUsername,
     password,
     setPassword,
-  }), []);
+    searchType,
+    setSearchType,
+    searchString,
+    setSearchString,
+    searchResults,
+    executeSearch,
+  }), [username, password, searchType, searchString, searchResults, executeSearch]);
+
   return (
     <RecipesContext.Provider value={ store }>
       {children}
