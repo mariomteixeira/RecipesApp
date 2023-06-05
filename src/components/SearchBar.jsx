297ace444495a4fe8f5585a1a98b3b2fdc7635ca
Victor Schmoegel <victorschmoegel@gmail.com>
2023-06-05 18:01:55 -0300

    Lint req 10

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -2,29 +2,29 @@ function SearchBar() {
   return (
     <>
       <button
-      className="search-bar"
-      data-testid="ingredient-search-radio"
-      type="radio"
+        className="search-bar"
+        data-testid="ingredient-search-radio"
+        type="radio"
       >
         Ingrediente
       </button>
       <button
-      className="search-bar"
-      data-testid="name-search-radio"
-      type="radio"
+        className="search-bar"
+        data-testid="name-search-radio"
+        type="radio"
       >
         Name
       </button>
       <button
-      className="search-bar"
-      data-testid="first-letter-search-radio"
-      type="radio"
+        className="search-bar"
+        data-testid="first-letter-search-radio"
+        type="radio"
       >
         First Letter
       </button>
       <button
-      className="search-bar"
-      data-testid="exec-search-btn"
+        className="search-bar"
+        data-testid="exec-search-btn"
       >
         Executar
       </button>
