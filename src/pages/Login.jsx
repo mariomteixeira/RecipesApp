337a6e1d0f690e17ed04a0c924915a67e5daa1ca
Joao Astolfi <jpastolfi@gmail.com>
2023-06-05 22:46:06 +0100

    Término da página de Login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,17 +1,22 @@
 import { useContext } from 'react';
+import { useHistory } from 'react-router-dom/cjs/react-router-dom.min';
 import RecipesContext from '../context/RecipesContext';
 
 export default function Login() {
+  const history = useHistory();
   const {
-    username,
-    setUsername,
-    password,
-    setPassword,
+    email,
+    setEmail,
+    passwordLength,
+    setPasswordLength,
   } = useContext(RecipesContext);
   const enableBtn = () => {
     const emailRegex = /^[a-z0-9.]+@[a-z0-9]+\.[a-z]+(\.[a-z]+)?$/i;
-    const passwordRegex = /^.{6,}$/;
-    return emailRegex.test(username) && passwordRegex.test(password);
+    return emailRegex.test(email) && passwordLength.length > Number('6');
+  };
+  const handleClick = () => {
+    localStorage.setItem('user', JSON.stringify({ email }));
+    history.push('/meals');
   };
   return (
     <div>
@@ -20,7 +25,7 @@ export default function Login() {
           data-testid="email-input"
           placeholder="Username"
           type="text"
-          onChange={ (({ target }) => setUsername(target.value)) }
+          onChange={ (event) => setEmail(event.target.value) }
         />
       </label>
       <label htmlFor="">
@@ -28,15 +33,16 @@ export default function Login() {
           data-testid="password-input"
           id=""
           name=""
-          onChange={ ((event) => setPassword(event.target.value)) }
+          onChange={ ((event) => setPasswordLength(event.target.value)) }
           placeholder="Password"
           type="password"
         />
       </label>
       <button
         data-testid="login-submit-btn"
-        type="button"
         disabled={ !enableBtn() }
+        onClick={ () => handleClick() }
+        type="button"
       >
         Enter
       </button>
