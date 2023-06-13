4aacb7c38b6355b5a1095d183978674bacaed761
mariomteixeira <mariomarcio12324@gmail.com>
2023-06-13 17:31:34 -0300

    Atualizando todos os testes para novo renderWithRouter.

--- src/tests/DoneRecipes.test.js
+++ src/tests/DoneRecipes.test.js
@@ -1,4 +1,17 @@
+import { screen } from '@testing-library/react';
+import { meals } from '../../cypress/mocks/meals';
+import App from '../App';
+import { renderWithRouter } from '../helpers/renderWithRouter';
+
 describe('Testando o componente DoneRecipes', () => {
-  it('Testando se a página contém um título', () => {
+  it('Testando se a página contém um título', async () => {
+    jest.spyOn(global, 'fetch');
+    global.fetch.mockResolvedValue({
+      json: jest.fn()
+        .mockResolvedValueOnce(meals),
+    });
+    const { history } = renderWithRouter(<App />, { initialEntries: ['/done-recipes'] });
+    await screen.findByText('Done Recipes');
+    expect(history.location.pathname).toBe('/done-recipes');
   });
 });
--- src/tests/Profiler.test.js
+++ src/tests/Profiler.test.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import { screen } from '@testing-library/react';
+import { screen, act } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import { renderWithRouter } from '../helpers/renderWithRouter';
-import Profile from '../pages/Profile';
+import App from '../pages/Profile';
 
 describe('Teste o componente <Profile.js />', () => {
   beforeEach(() => {
@@ -10,15 +10,17 @@ describe('Teste o componente <Profile.js />', () => {
   });
 
   it('Verifica se a página contém as informações do usuário', () => {
-    const { history } = renderWithRouter(<Profile />, { route: '/profile' });
+    const { history } = renderWithRouter(<App />);
+    act(() => history.push('/profile'));
     const email = screen.getByTestId('profile-email');
     expect(email).toBeInTheDocument();
     const { pathname } = history.location;
-    expect(pathname).toBe('/');
+    expect(pathname).toBe('/profile');
   });
 
   it('Testa se a rota é alterada para a página de receitas feitas', () => {
-    const { history } = renderWithRouter(<Profile />, { route: '/profile' });
+    const { history } = renderWithRouter(<App />);
+    act(() => history.push('/profile'));
     const doneRecipes = screen.getByTestId('profile-done-btn');
     userEvent.click(doneRecipes);
     const { pathname } = history.location;
@@ -26,7 +28,8 @@ describe('Teste o componente <Profile.js />', () => {
   });
 
   it('Testa se a rota é alterada para a página de receitas favoritas', () => {
-    const { history } = renderWithRouter(<Profile />, { route: '/profile' });
+    const { history } = renderWithRouter(<App />);
+    act(() => history.push('/profile'));
     const favoriteRecipes = screen.getByTestId('profile-favorite-btn');
     userEvent.click(favoriteRecipes);
     const { pathname } = history.location;
@@ -34,7 +37,8 @@ describe('Teste o componente <Profile.js />', () => {
   });
 
   it('Testa se ao clicar no botão de sair, a rota é alterada para a página de login', () => {
-    const { history } = renderWithRouter(<Profile />, { route: '/profile' });
+    const { history } = renderWithRouter(<App />);
+    act(() => history.push('/profile'));
     const logout = screen.getByTestId('profile-logout-btn');
     userEvent.click(logout);
     const { pathname } = history.location;
